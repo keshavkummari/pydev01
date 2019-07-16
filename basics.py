888816aa7e7549fd9681c581b052a1eb6727b796
kchennakesavulu <chennakesavulu.kummari@outlook.com>
2019-07-16 07:37:16 +0530

    understanding-of-variables

---
+++ basics.py
@@ -0,0 +1,16 @@
+
+# Creating of Variables
+
+firstname = 'Guido'
+middle_name = "Van"
+LastName = 'Rossum'
+
+# Accessing Variables
+
+print(firstname)
+print(middle_name)
+print(LastName)
+
+print(type(firstname))
+
+
